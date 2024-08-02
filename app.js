263ab21bce055a4206a05615734460ba1e005ff4
Evanshu24 <evanshuj@gmail.com>
2024-08-03 00:27:39 +0530

    first lecture

---
+++ app.js
@@ -0,0 +1,7 @@
+const heading=React.createElement(
+  "h1",
+  {className:"heading",id:"heading"},
+  "This is my first react project"
+);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
